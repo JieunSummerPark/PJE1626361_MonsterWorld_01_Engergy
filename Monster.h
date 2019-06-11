75428de71f9728bb536fde44c000ab88b45d23b5
JieunSummerPark <parkj2@student.swosu.edu>
2019-06-12 04:33:49 +0900

    주석 달았음

--- Monster.h
+++ Monster.h
@@ -5,7 +5,7 @@
 class Monster {
 	string name, icon;		// 몬스터 이름과 화면 출력용 아이콘
 	int x, y, nItem;		// 현재 위치와 먹은 아이템 수
-	int nEnergy;			// 에너지
+	int nEnergy;			// 에너지를 저장하는 속성
 
 	void clip(int maxx, int maxy) {
 		if (x < 0) x = 0;
@@ -17,12 +17,15 @@ class Monster {
 		if (map[y][x] == 1) {
 			map[y][x] = 0;
 			nItem++;
-			nEnergy += 8;
+			nEnergy += 8;		// 아이템을 먹으면 에너지가 8 증가됨
+		}
+		else if (nEnergy>0) {	// 에너지의 최솟값은 0
+			nEnergy -= 1;		// 아이템을 먹지 못하면 에너지가 1 감소
 		}
 	}
 public:
 	Monster(string n = "나괴물", string i = "※", int px = 0, int py = 0)
-		: name(n), icon(i), x(px), y(py), nItem(0), nEnergy(100) {}
+		: name(n), icon(i), x(px), y(py), nItem(0), nEnergy(100) {} /*맨 처음에는 모든 몬스터 객체의 에너지가 100*/
 	~Monster() { cout << "\t" << name << icon << " 물러갑니다~~~\n"; }
 
 	void draw(Canvas & canvas) { canvas.draw(x, y, icon); }
